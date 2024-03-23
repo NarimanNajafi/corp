2ea9c1fa69b785c38b64a5d432ddc45b34b3198b
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2024-03-23 15:45:12 +0330

    image added

--- src/app/reliability/page.jsx
+++ src/app/reliability/page.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Image from "next/image"
+import reliabilityImg from '../../../public/reliability.jpg'
 
 function page() {
   return (
-    <div>Reloability page!</div>
+    <div className=" absolute -z-10 inset-0">Reloability page!
+      <Image src={reliabilityImg} 
+      alt="reliabilty img" 
+      fill 
+      style={{objectFit:'cover'}}/>
+    </div>
   )
 }
 
