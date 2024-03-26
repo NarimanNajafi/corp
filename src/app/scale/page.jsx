b47c3b99ea1c6ccedb370f0fd1084fc41deb707b
nariman najafi <narimannajafi@narimans-MacBook-Air.local>
2024-03-26 12:23:11 +0330

    bug fixed and bg img added

--- src/app/scale/page.jsx
+++ src/app/scale/page.jsx
@@ -1,11 +1,13 @@
-import Image from "next/image"
+import Hero from '@/components/hero'
 import scaleImg from '../../../public/scale.jpg'
 
 function page() {
   return (
-    <div className=" absolute -z-10 inset-0">Scale page!
-      <Image src={scaleImg} alt="scale img" fill style={{objectFit:'cover'}}/> 
-    </div>
+    <Hero
+    imgData={scaleImg}
+    imgAlt="Steel factory"
+    title="Scale your app to Infinity"
+  />
   )
 }
 
